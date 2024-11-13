1551f2e88dfbc636d2f2c5c21a19a8e0b4164513
jchaselubitz <jchaselubitz@gmail.com>
2024-11-13 15:14:53 +0100

    switch back to config ts

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+// Import the necessary modules
+import withPWA from 'next-pwa';
+/** @type {NextConfig} */
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  disable: process.env.NEXT_PUBLIC_CONTEXT === 'development',
+  // skipWaiting: false,
+  // cacheOnFrontEndNav: true,
+});
+
+const nextConfig = {
+  ...pwaConfig,
+
+  // reactStrictMode: false,
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true,
+  //   },
+  // },
+};
+
+export default nextConfig;
