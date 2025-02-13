ebe54c9a0051aa32d7b4163ef4704d5fa93b8e3a
jchaselubitz <jchaselubitz@gmail.com>
2025-02-13 14:47:33 +0100

    update plug

--- components/developer_plug.tsx
+++ components/developer_plug.tsx
@@ -8,15 +8,12 @@ const DeveloperPlug: React.FC = () => {
       rel="noopener noreferrer"
       className="flex flex-col w-full p-2 text-sm border rounded-lg bg-slate-50 hover:shadow-lg cursor-pointer mt-2 hover:underline"
     >
-      <span className="hidden md:flex items-center gap-2">
-        Created by <img src="/icons/cooperativ_logo.png" className="h-6" />
-        Cooperativ Labs
-      </span>{' '}
-      <span className="md:hidden flex items-center  gap-2">
-        <img src="/icons/cooperativ_logo.png" className="h-6" />
-        Cooperativ Labs
+      <span className="flex items-center gap-2">Created by Jake</span>
+      <span className="items-center gap-2">
+        <span className="text-xs flex gap-2 items-center">
+          Cooperativ Labs <img src="/icons/cooperativ_logo.png" className="h-6" />
+        </span>
       </span>
-      <span className="flex items-center text-xs gap-2">Custom software development</span>
     </a>
   );
 };
