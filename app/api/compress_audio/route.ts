458ebfd730930f80d7afa69649eaad0e120afe76
jchaselubitz <jchaselubitz@gmail.com>
2025-02-18 10:23:39 +0100

    try adding to the netlify toml

--- app/api/compress_audio/route.ts
+++ app/api/compress_audio/route.ts
@@ -9,14 +9,10 @@ import os from 'os';
 if (!ffmpegStatic) {
   throw new Error('ffmpeg binary path is null');
 }
-
 if (!fs.existsSync(ffmpegStatic)) {
   throw new Error(`ffmpeg binary not found at: ${ffmpegStatic}`);
-}
-if (ffmpegStatic) {
-  ffmpeg.setFfmpegPath(ffmpegStatic);
 } else {
-  throw new Error(`ffmpeg binary not found at: ${ffmpegStatic}`);
+  ffmpeg.setFfmpegPath(ffmpegStatic);
 }
 
 export async function POST(req: Request) {
