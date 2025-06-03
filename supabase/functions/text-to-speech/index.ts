d9018ffaa028a89c8d32bd18cdad605270ff9516
jchaselubitz <jchaselubitz@gmail.com>
2025-06-03 13:57:40 +0200

    fix TTS issue

--- supabase/functions/text-to-speech/index.ts
+++ supabase/functions/text-to-speech/index.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from 'https://deno.land/x/openai@v4.69.0/mod.ts';
-import { createClient } from 'jsr:@supabase/supabase-js@2';
 import { corsHeaders } from '../_shared/cors.ts';
 import { Langfuse } from 'https://esm.sh/langfuse';
+import { supabaseServiceRole } from '../_shared/supabaseAdmin.ts';
 
 Deno.serve(async (req) => {
   if (req.method === 'OPTIONS') {
@@ -34,11 +34,6 @@ Deno.serve(async (req) => {
     },
   });
 
-  const supabaseAdminClient = createClient(
-    Deno.env.get('SUPABASE_URL') ?? '',
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
-  );
-
   const openai = new OpenAI({ apiKey: userApiKey });
 
   span.update({
@@ -55,7 +50,7 @@ Deno.serve(async (req) => {
   const arrayBuffer = await mp3.arrayBuffer();
   const uint8Array = new Uint8Array(arrayBuffer);
 
-  const { data: storageData, error } = await supabaseAdminClient.storage
+  const { data: storageData, error } = await supabaseServiceRole.storage
     .from(bucketName)
     .upload(fileName, uint8Array, {
       contentType: 'audio/mpeg',
